7f39c12049801caccc61bac93ad364aa98647ebb
22CS020samarthdhol <120238118+22CS020samarthdhol@users.noreply.github.com>
2023-04-30 10:05:55 +0530

    Add files via upload

--- creditcard.cpp
+++ creditcard.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 //Create a function to check if a credit card number is valid
-bool is_Credit_Card_Valid(string cardNumber)
+bool is_Credit_Card_Valid(string cardNum)
 {
     int sum = 0; //Initialize the sum variable to zero
     bool alternate = false; //Initialize the alternate variable to false
-    for (int i = cardNumber.length() - 1; i >= 0; i--)
+    for (int i = cardNum.length() - 1; i >= 0; i--)
     {
-        int digit = cardNumber[i] - '0'; // Convert the character digit to an integer
+        int digit = cardNum[i] - '0'; // Convert the character digit to an integer
         if (alternate)
         {
             digit *= 2; // Double the digit if it is in an alternate position
@@ -26,10 +26,10 @@ bool is_Credit_Card_Valid(string cardNumber)
 
 int main()
 {
-    string cardNumber; // Declare a string variable to store the credit card number
+    string cardNum; // Declare a string variable to store the credit card number
     cout << "Enter credit card number: ";
-    cin >> cardNumber; // Read the credit card number from the user
-    if (is_Credit_Card_Valid(cardNumber)) // If the credit card number is valid, print a message
+    cin >> cardNum; // Read the credit card number from the user
+    if (is_Credit_Card_Valid(cardNum)) // If the credit card number is valid, print a message
     {
         cout<<"-------------------------------------------------"<<endl;
         cout <<endl<< "CREDIT CARD NUMBER IS VALID." << endl <<endl;
